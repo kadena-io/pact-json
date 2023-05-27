f203f2f05f6c1b7393d6512b458bb6a8fe8791aa
Lars Kuhtz <lars@kadena.io>
2023-05-27 13:09:50 -0700

    add missing HasCallStack constraints

--- src/Pact/JSON/Yaml.hs
+++ src/Pact/JSON/Yaml.hs
@@ -24,30 +24,48 @@ import qualified Data.ByteString as B
 import qualified Data.Text as T
 import qualified Data.Yaml as Y
 
+import GHC.Stack
+
 import qualified Pact.JSON.Encode as J
 
 -- -------------------------------------------------------------------------- --
 -- YAML Utils
 
-encodeYaml :: J.Encode a => a -> B.ByteString
+encodeYaml :: HasCallStack => J.Encode a => a -> B.ByteString
 encodeYaml = Y.encode
   . either (error . show) id
   . Y.decodeEither' @Value
   . J.encodeStrict
 
-encodeYamlWith :: J.Encode a => Y.EncodeOptions -> a -> B.ByteString
+encodeYamlWith
+  :: HasCallStack
+  => J.Encode a
+  => Y.EncodeOptions
+  -> a
+  -> B.ByteString
 encodeYamlWith opts = Y.encodeWith opts
   . either (error . show) id
   . Y.decodeEither' @Value
   . J.encodeStrict
 
-encodeYamlFile :: J.Encode a => FilePath -> a -> IO ()
+encodeYamlFile
+  :: HasCallStack
+  => J.Encode a
+  => FilePath
+  -> a
+  -> IO ()
 encodeYamlFile file = Y.encodeFile file
   . either (error . show) id
   . Y.decodeEither' @Value
   . J.encodeStrict
 
-encodeYamlFileWith :: J.Encode a => Y.EncodeOptions -> FilePath -> a -> IO ()
+encodeYamlFileWith
+  :: HasCallStack
+  => J.Encode a
+  => Y.EncodeOptions
+  -> FilePath
+  -> a
+  -> IO ()
 encodeYamlFileWith opts file = Y.encodeFileWith opts file
   . either (error . show) id
   . Y.decodeEither' @Value
