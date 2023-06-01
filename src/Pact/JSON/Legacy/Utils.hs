e43073d0b8d89d9b300980913b842f4be339846d
Lars Kuhtz <lars@kadena.io>
2023-06-01 09:07:00 -0700

    drop ToJSONKey constraint from legacy map conversions

--- src/Pact/JSON/Legacy/Utils.hs
+++ src/Pact/JSON/Legacy/Utils.hs
@@ -39,31 +39,6 @@ import Pact.JSON.Legacy.Hashable
 import qualified Pact.JSON.Legacy.HashMap as LHM
 
 -- -------------------------------------------------------------------------- --
--- Utils
-
--- | Transform a list of key value pairs with a textual JSON key encoding into a
--- list of pairs with text keys.
---
--- It is an error to call this function on pairs with non-textual keys.
---
--- Often, instead of forcing Text Keys we would also just require
--- 'LegacyHashable' for the key type, which would avoid the 'error'. However, we
--- think (and have evidence), that a non-textual encoding is almost always
--- uninteded and enforcing a textual encoding catches those issues.
---
-enforceTextKeys
-  :: forall k v
-  . HasCallStack
-  => Typeable k
-  => ToJSONKey k
-  => [(k, v)]
-  -> [(T.Text, v)]
-enforceTextKeys = case toJSONKey of
-  ToJSONKeyText f _ -> fmap (first (AK.toText . f))
-  _ -> error $ "Pact.Utils.Json: failed to JSON encode non-textual legacy key of type " <> show (typeRep (Proxy @k))
-{-# INLINE enforceTextKeys #-}
-
--- -------------------------------------------------------------------------- --
 -- Aeson KeyMap Conversion
 
 -- | Convert a 'AKM.KeyMap' that has a textual JSON key into LegacyHashMap which
@@ -88,11 +63,10 @@ legacyKeyMap = LHM.fromList . fmap (first AK.toText) . AKM.toList
 --
 legacyMap
   :: HasCallStack
-  => Typeable a
-  => ToJSONKey a
-  => M.Map a b
+  => (a -> T.Text)
+  -> M.Map a b
   -> LHM.HashMap T.Text b
-legacyMap = LHM.fromList . enforceTextKeys . M.toList
+legacyMap keyToText = LHM.fromList . fmap (first keyToText) . M.toList
 {-# INLINE legacyMap #-}
 
 -- | A version of legacyMap that does not enforce textual keys but instead
@@ -108,26 +82,18 @@ legacyMap_ = LHM.fromList . M.toList
 -- | Convert a 'HM.HashMap.Strict' that has a textual JSON key into HashMap with
 -- a Legacy Key.
 --
--- It is an error if the key type is not encoded as JSON text.
---
--- Instead of forcing text Keys we would also just require 'LegacyHashable' for
--- the key type, which would avoid the 'error'. However, we think (and have
--- evidence), that a non-textual encoding is almost always uninteded and
--- enforcing a textual encoding catches those issues.
---
--- That said, there are occurences of Hashmaps with non-textual keys (even for
--- key types that have ToJSONKey instances) where the map is encoded as list of
--- pairs (lists of length 2), which is the default encoding in aeson. For these
+-- There are occurences of Hashmaps with non-textual keys (even for key types
+-- that have ToJSONKey instances) where the map is encoded as list of pairs
+-- (lists of length 2), which is the default encoding in aeson. For these
 -- (probaby unintentional) special cases we also provide 'legacyHashMap_' which
--- has  'LegacyHashable' constraint for the key type.
+-- has 'LegacyHashable' constraint for the key type.
 --
 legacyHashMap
   :: HasCallStack
-  => Typeable a
-  => ToJSONKey a
-  => HM.HashMap a b
+  => (a -> T.Text)
+  -> HM.HashMap a b
   -> LHM.HashMap T.Text b
-legacyHashMap = LHM.fromList . enforceTextKeys . HM.toList
+legacyHashMap keyToText = LHM.fromList . fmap (first keyToText) . HM.toList
 {-# INLINE legacyHashMap #-}
 
 -- | A version of legacyMap that does not enforce textual keys but instead
@@ -138,7 +104,7 @@ legacyHashMap_ = LHM.fromList . HM.toList
 {-# INLINE legacyHashMap_ #-}
 
 -- -------------------------------------------------------------------------- --
--- Tools
+-- Sorting Tools
 
 -- | Sort a list of 'T.Text' using the legacy JSON property sorting.
 --
